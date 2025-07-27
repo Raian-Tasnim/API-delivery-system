ba297e1042b04fe65aac766ef962f5576150c412
Raian-Tasnim <114634322+Raian-Tasnim@users.noreply.github.com>
2025-07-27 17:19:16 +0600

    Add files via upload

---
+++ DonationManageProject/Controllers/DonationController.cs
@@ -0,0 +1,59 @@
+﻿using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DonationManageProject.Controllers
+{
+    public class DonationController : ApiController
+    {
+        [HttpGet]
+        [Route("api/donations")]
+        public HttpResponseMessage Donations()
+        {
+            try
+            {
+                var data = DonerService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("api/campaigns")]
+        public HttpResponseMessage Campaigns()
+        {
+            try
+            {
+                var data = CampaignService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("api/donationlist")]
+        public HttpResponseMessage DonationList()
+        {
+            try
+            {
+                var data = DonationService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+    }
+}
